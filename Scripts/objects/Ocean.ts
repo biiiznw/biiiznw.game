f19175cc04c1e0378f30374a996df3fe1942778d
biiiznw <biiiznw@gmail.com>
2020-02-15 11:03:01 -0500

    update

---
+++ Scripts/objects/Ocean.ts
@@ -0,0 +1,52 @@
+module objects
+{
+    export class Ocean extends GameObject
+    {
+        // private instance menbers
+        private _verticalSpeed?: number; // 5 px per frame
+
+        //public properities
+
+        //constructor
+        constructor(){
+            super("./Assets/images/ocean.gif");
+            this.Start();
+        }
+
+        //provate method
+        protected _checkBounds():void{
+            if(this.position.y >= 0)
+            {
+                this.Reset();
+            }
+
+        }
+
+        private _move():void
+        {
+
+            this.position = Vector2.add(this.position, this.velocity);
+        }
+
+        //public method
+        public Start():void
+        {
+            this._verticalSpeed = 5;
+            this.velocity = new Vector2(0, this._verticalSpeed);
+            this.Reset();
+
+        }
+
+        public Update():void
+        {
+            this._move();
+            this._checkBounds();
+
+        }
+
+        public Reset():void{
+            this.position.y = -960;
+
+        }
+    }
+}
